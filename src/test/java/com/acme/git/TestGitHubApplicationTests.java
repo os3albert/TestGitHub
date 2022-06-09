091472f080f7f8f4858b1133f89e4b060907a245
Albert <47365826+os3albert@users.noreply.github.com>
2022-06-09 15:10:01 +0200

    primo commit

---
+++ src/test/java/com/acme/git/TestGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
