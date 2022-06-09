02318dd20c80258c655a49dae8efe04a72cbaa8d
fraug <fraug@192.168.178.116>
2022-06-09 15:16:11 +0200

    Variazione nel file

---
+++ src/main/java/com/acme/git/test/Test.java
@@ -0,0 +1,16 @@
+package com.acme.git.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Test {
+	
+	private String  nome;
+	private String cognome;
+
+}
